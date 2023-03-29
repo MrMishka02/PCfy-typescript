410ba02095a4655504a1bbf7fdf3c713961c2a50
MrMishka02 <m.gogia@hotmail.com>
2023-03-29 12:27:33 +0400

    docs: added teams and positions data

---
+++ src/positions.js
@@ -0,0 +1,87 @@
+const positions = [
+  {
+    id: 1,
+    team_id: 1,
+    position: 'ჯუნიორ დეველოპერი',
+  },
+  {
+    id: 2,
+    team_id: 1,
+    position: 'მიდლ დეველოპერი',
+  },
+  {
+    id: 3,
+    team_id: 1,
+    position: 'სენიორ დეველოპერი',
+  },
+  {
+    id: 4,
+    team_id: 1,
+    position: 'Full-Stack დეველოპერი',
+  },
+  {
+    id: 5,
+    team_id: 2,
+    position: 'HR ასისტენტი',
+  },
+  {
+    id: 6,
+    team_id: 2,
+    position: 'HR სპეციალისტი',
+  },
+  {
+    id: 7,
+    team_id: 2,
+    position: 'HR მენეჯერი',
+  },
+  {
+    id: 8,
+    team_id: 3,
+    position: 'მოლარე-ოპერატორი',
+  },
+  {
+    id: 9,
+    team_id: 3,
+    position: 'გაყიდვების აგენტი',
+  },
+  {
+    id: 10,
+    team_id: 3,
+    position: 'გაყიდვების მენეჯერი',
+  },
+  {
+    id: 11,
+    team_id: 4,
+    position: 'UI დიზაინერი',
+  },
+  {
+    id: 12,
+    team_id: 4,
+    position: 'UX დიზაინერი',
+  },
+  {
+    id: 13,
+    team_id: 4,
+    position: 'გრაფიკული დიზაინერი',
+  },
+  {
+    id: 14,
+    team_id: 5,
+    position: 'მარკეტინგის სპეციალისტი',
+  },
+  {
+    id: 15,
+    team_id: 5,
+    position: 'მარკეტინგის კონსულტანტი',
+  },
+  {
+    id: 16,
+    team_id: 5,
+    position: 'მარკეტინგის ანალიტიკოსი',
+  },
+  {
+    id: 17,
+    team_id: 5,
+    position: 'მარკეტინგის მენეჯერი',
+  },
+]
