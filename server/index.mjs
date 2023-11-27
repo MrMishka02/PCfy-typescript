2ffeda3ab3bde5542cfa9c2b16cdbe8e93117952
Mikheil Gogia <m.gogia@hotmail.com>
2023-11-28 00:37:48 +0400

    docs: added conection to mongo database and post to the server

--- server/index.mjs
+++ server/index.mjs
@@ -1,33 +1,51 @@
-import express from 'express'
+import express from 'express';
+import pkg from 'mongoose';
+const { connect, connection, Schema, model } = pkg;
+import pkg2 from 'body-parser';
+const { json } = pkg2;
 import cors from 'cors'
-import bodyParser from 'body-parser'
 
-const app = express()
+const app = express();
+const port = 3001;
 app.use(cors())
-app.use(bodyParser.json())
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-// app.get('/create', (req, res) => {
-//   res.send({
-//     "firstName": req.body.firstName,
-//     "surName": req.body.surName,
-//     "team": req.body.team,
-//     "position": req.body.position,
-//     "email": req.body.email,
-//     "phoneNumber": req.body.phoneNumber
-// })
-// })
+connect('mongodb+srv://mrmishka:Coding0205@cluster0.fy5yilt.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = connection;
 
-app.post('/create', urlencodedParser, (req, res) => {
-  console.log(req.body)
-  res.send({
-      "Firstname": req.body.firstName,
-      "Lastname": req.body.surName,
-      "Team": req.body.team,
-      "Position": req.body.position,
-      "Email": req.body.email,
-      "Phonenumber": req.body.phoneNumber
-  })
-})
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
-app.listen(3001, console.log('server started!'))
+const personalInfoSchema = new Schema({
+  firstName: String,
+  surName: String,
+  team: String,
+  position: String,
+  email: String,
+  phoneNumber: String,
+  // Add more fields as needed
+});
+
+const PersonalInfo = model('PersonalInfo', personalInfoSchema);
+
+app.use(json());
+
+app.post('/create', (req, res) => {
+  const data = req.body;
+  const newPersonalInfo = new PersonalInfo(data);
+
+  newPersonalInfo.save()
+    .then(() => {
+      console.log('Data created and saved to MongoDB');
+      res.status(201).send('Data created and saved to MongoDB');
+    })
+    .catch((err) => {
+      console.error('Error saving data to MongoDB:', err);
+      res.status(500).send('Internal Server Error');
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
