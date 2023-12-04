8bc2d88d7348bd4f345309fa0a503d003dee06f5
Mikheil Gogia <m.gogia@hotmail.com>
2023-12-04 15:38:20 +0400

    chore: corrected some styles

--- src/pages/InfoPage/InfoPage.tsx
+++ src/pages/InfoPage/InfoPage.tsx
@@ -26,7 +26,7 @@ const InfoPage = () => {
             className='w-[36rem] sm:w-[22.375rem]'
           />
         </div>
-        <div className='flex w-[50%] sm:mt-4 sm:w-full'>
+        <div className='flex ml-20 w-[50%] sm:mt-4 sm:w-full'>
           <div className='ml-4 mt-8 w-[30%] sm:ml-10 sm:mt-0 sm:w-[45%]'>
             <InfoLabel text={'სახელი: '} />
             <InfoLabel text={'თიმი: '} />
