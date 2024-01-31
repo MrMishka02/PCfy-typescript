1d80b724dd216792e2c7a1fe50b59fe57ad71fe3
Mikheil Gogia <m.gogia@hotmail.com>
2024-01-31 14:48:36 +0400

    chore: corrected some mobile responsive styles

--- src/components/DeleteModal/DeleteModal.tsx
+++ src/components/DeleteModal/DeleteModal.tsx
@@ -48,13 +48,13 @@ const DeleteModal = ({
       className='fixed inset-0 flex items-center justify-center
     bg-black bg-opacity-25 backdrop-blur-sm sm:bg-[#FFFFFF]'
     >
-      <div className='flex h-[33.56rem] w-[52.9rem] flex-col sm:h-[42rem] sm:w-[24.375rem] sm:overflow-y-scroll'>
+      <div className='flex h-[33.56rem] w-[52.9rem] flex-col sm:h-[22rem] lg:w-[90%] sm:overflow-y-scroll'>
         <div className='flex flex-col items-center justify-center rounded-lg bg-white p-2'>
-          <img src={redcross} alt='Delete' className='w-[12rem]' />
-          <p className='text-2xl font-[700] text-[#292929] sm:w-[12.5rem] sm:text-center'>
+          <img src={redcross} alt='Delete' className='w-[12rem] sm:w-[8rem]' />
+          <p className='text-2xl font-[700] text-[#292929] lg:w-[90%] lg:text-center sm:text-base'>
             დარწმუნებული ხართ რომ გსურთ ჩანაწერის წაშლა?
           </p>
-          <div className='flex gap-10 mt-20 mb-10 h-[3.75rem] w-[18.5625rem] text-[1.25rem] leading-6 sm:mt-[12rem] sm:mb-10 sm:w-[22.375rem]'>
+          <div className='flex gap-10 mt-20 mb-10 h-[3.75rem] w-[18.5625rem] text-[1.25rem] leading-6 sm:mt-[3rem] sm:mb-10 sm:w-[18.375rem]'>
             <Button
               text={'დიახ'}
               path='/entries-list'
